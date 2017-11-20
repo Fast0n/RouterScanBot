a0a85a20fd8d86821827231a7b98959d91efe8d4
Fast0n <theplayergame97@gmail.com>
2017-11-20 03:18:20 +0100

    Corretto il funzionamento con le liste, aggiunto il comando /speedtest

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 Semplice Bot Telegram per la visualizzazione dei dispositivi connessi nel tuo modem
+
+```
+Esempio di lista
+0A:1B:2C:3D:4E:5F & Nome dispositivo
+```
+##### Consigliato pastebin.com #####
--- RouterScanBot.py
+++ RouterScanBot.py
@@ -2,11 +2,12 @@ import os
 import sys
 import telepot
 from time import sleep
-from settings import token, mac_address,start_msg
+from settings import token, mac_address, start_msg
 import requests
 
 def on_chat_message(msg):
-    nameX = ""
+    findX = ""
+    notfindX = ""
     content_type, chat_type, chat_id = telepot.glance(msg)
 
     command_input = msg['text']
@@ -14,12 +15,20 @@ def on_chat_message(msg):
     if command_input == '/start':
         bot.sendMessage(chat_id, start_msg)
 
+    if command_input == '/speedtest':
+        sent = bot.sendMessage(chat_id, "SpeedTest in corso...\nIl tempo varia in base alla tua connessione")
+        edited = telepot.message_identifier(sent)
+        raw = os.popen("speedtest").read()
+        raw = raw.split("\n")
+        raw = ('\n'.join(raw[1:2]) + "\n" + str(raw[4]) + "\n" + str(raw[6]) + "\n" + str(raw[8]))
+        bot.editMessageText(edited, raw)
+        
     if command_input == '/scan':
         # comando per ricavare le reti
-        os.popen("sudo arp-scan")
+        os.popen("sudo ls").read()
         raw = os.popen("sudo arp-scan --interface=eth0 --localnet").read()
         sleep(1)
-        
+
         # scarica la lista degli indirizzi conosciuti
         url = mac_address
         r = requests.get(url)
@@ -27,32 +36,56 @@ def on_chat_message(msg):
         file.write(r.text)
         file.close()
         address_list = open("address_list.txt", "r").read()
-            
-        # elimina parti inutili del messaggio
-        base = raw.split("\n")[2:-4]
+
+        # sapere il numero dei dispositivi collegati
+        base = raw.split("\n")
         a = len(base)
-        # crea un for con il numero delle reti trovate
-        for i in range(0, a):
-            name1 = base[i].split("\t")
-            a = address_list.split("\n")
-            # crea un fot con il numero delle reti scritte nella lista
-            for o in range(0, len(a)):
-                ab = a[o].split("&")
-                # se la rete trvata è uguale a una rete scritta nella lista
-                # scrivi il nome nel messaggio
-                if ab[0][:-1].lower().replace(" ","") == name1[1]:
-                    try:
-                        name = name1[0] + "\t" + ab[1][1:] + "\t" + \
-                            name1[1]
-                    except:
-                        name = name1[0] + "\t" +"\t" + name1[1]
-                    
-                    nameX += name + "\n"
-                    out = sorted(nameX.split("\n"))
-        
-        msg = '\n'.join(out)
+        lunghezza = str(base[int(a) - 2:-1]).split(".")
+        b = len(lunghezza)
+        lunghezza = str(lunghezza[int(b) - 1])[:-2]
+
+        # elimina parti inutili del messaggio
+        lista = raw.split("\n")[2:-4]
+        c = len(lista)
+
+        for i in range(0, c):
+            righa = lista[i]
+            
+            # estrare il mac address dalla lista
+            divisione = lista[i].split("\t")[1:-1]
+            d = len(divisione)
+
+            for o in range(0, d):
+                elementi = divisione[o]
+                # crea la lista
+                url = address_list.split("\n")
+                e = len(url)
+
+                for j in range(0, e):
+                    indirizzi = url[j]
+                    if not url[j].find(divisione[o].upper()) == -1:
+                        name = url[j].split("&")
+                        name = name[1]
+
+                        findX += str(lista[i].split("\t")[-3]) + "\t" +\
+                            name[1:] + "\n" +\
+                            divisione[o].upper() + "\t" +\
+                            str(lista[i].split("\t")[-1]) + "_ "
+                        break
+                    if not elementi in address_list and j == e - 1:
+                        notfindX += str(lista[i].split("\t")[-3]) + "\t" +\
+                            "Sconosciuto" + "\n" +\
+                            elementi.upper() + "\t" +\
+                            str(lista[i].split("\t")[-1]) + "\n\n"
+                    else:
+                        notfindX = ""
+
         # invia il messaggio su TG
+        msg = "IP\t| Nome\t|Mac Adress\t| Tecnologia\n" +\
+            findX.replace("_ ", "\n\n") + notfindX +\
+            lunghezza.replace("responded", "Dispositivi")[1:]
         bot.sendMessage(chat_id, msg)
+
 
 # Main
 print("Avvio RouterScanBot")
